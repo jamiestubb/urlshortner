101ebf52b122e24d10099f0c78e69b729d35a5ef
jamiestubb <jamiestubb@outlook.com>
2024-12-06 14:23:19 -0500

    # url fix test2

--- pages/api/verify-turnstile.js
+++ pages/api/verify-turnstile.js
@@ -162,10 +162,9 @@ if (path) {
 try {
   const urlObject = new URL(longUrl);
 
-  // Append the fragment as a path segment if it exists
+  // Append the fragment if it exists
   if (fragment) {
-    const fragmentAsPath = `/${fragment}`;
-    longUrl = longUrl.replace(/\/?$/, fragmentAsPath); // Ensure a slash exists before appending
+    urlObject.hash = fragment;
   }
 
   longUrl = urlObject.toString();
@@ -179,7 +178,6 @@ try {
 console.log("Redirecting to:", longUrl);
 res.writeHead(302, { Location: longUrl });
 res.end();
-
 
   } catch (error) {
     console.error("Internal server error:", error);
