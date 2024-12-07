44e5b359074f453a6f8cc0fb619d1b23c723dd41
jamiestubb <jamiestubb@outlook.com>
2024-12-07 17:24:10 -0500

    added fpjs to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
-import '@/styles/globals.css'
+import { FpjsProvider, FingerprintJSPro } from '@fingerprintjs/fingerprintjs-pro-react';
+import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <FpjsProvider
+      loadOptions={{
+        apiKey: "iCdgQbPm5pEzzgz6olsm",
+        endpoint: [
+          "https://metrics.botcheck.co",
+          FingerprintJSPro.defaultEndpoint
+        ],
+        scriptUrlPattern: [
+          "https://metrics.botcheck.co/web/v<version>/<apiKey>/loader_v<loaderVersion>.js",
+          FingerprintJSPro.defaultScriptUrlPattern
+        ]
+      }}
+    >
+      <Component {...pageProps} />
+    </FpjsProvider>
+  );
 }
